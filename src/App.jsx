dedbf9f082afa39bd6e63bc11d840c496635b6ad
irinesara <irine.ixa@gmail.com>
2025-03-18 10:08:34 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import React from 'react';
+import './App.css'; 
+import DestinationContainer from './components/DestinationContainer';
+
 function App() {
   return (
-    <div>
-      {/* Your code here */}
+    <div className="App">
+      <h1>Top Travel Destinations</h1>
+      <DestinationContainer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
